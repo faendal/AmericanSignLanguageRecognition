fc18f1bafe153fe85e5981bf49a094c4cfe65f1c
Juan José Borrero Mejía <juanjoborrero@yahoo.com>
2025-04-22 19:56:51 -0500

    Training advance

--- train_model.py
+++ train_model.py
@@ -1,14 +1,19 @@
 from ultralytics import YOLO
 
-# Cargar modelo base (pequeño para tu GPU)
-model = YOLO("yolov8n.pt")
+def main():
+    model = YOLO("yolov8n.pt")
+    
+    model.train(
+        data="C:/Users/juanj/OneDrive - UPB/NoEstructurados/AmericanSignLanguageRecognition/asl_dataset/data.yaml",
+        epochs=30,
+        imgsz=416,
+        batch=4,
+        name="asl_model",
+        device=0,
+    )
 
-# Entrenar
-model.train(
-    data="asl_dataset/data.yaml",
-    epochs=30,
-    imgsz=416,
-    batch=4,
-    name="asl_model",
-    device=0,
-)
+if __name__ == "__main__":
+    import multiprocessing
+    multiprocessing.freeze_support()
+    main()
+
